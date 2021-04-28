bbf6e08afc249b24b3c19e6abb1a4dac7d4328b7
acalebwilson24 <caleb@natureslaboratory.co.uk>
2021-04-28 14:56:17 +0100

    added news page

---
+++ perch/templates/layouts/tags.php
@@ -0,0 +1,6 @@
+<?php
+
+perch_blog_post_tags(perch_layout_var('slug', true), ["cache"=>false]);
+
+
+?>
---
+++ perch/templates/pages/news.php
@@ -0,0 +1,31 @@
+<?php 
+if (!defined('PERCH_RUNWAY')) {
+     include($_SERVER['DOCUMENT_ROOT'] . '/perch/runtime.php');
+}
+
+// Report all PHP errors
+error_reporting(-1);
+
+// Same as error_reporting(E_ALL);
+ini_set('error_reporting', E_ALL);
+
+?>
+<?php perch_layout("global.header") ?>
+
+<main>
+    <div class="l-wrap">
+        <div class="l-restrict l-restrict--narrow">
+            <?php
+                if (perch_get("s")) {
+                    perch_blog_post(perch_get("s"));
+                } else {
+                    perch_blog_custom([
+                        "template" => "post_in_list_card.html",
+                        "count" => 10
+                    ]);
+                }
+            ?>
+        </div>
+    </div>
+</main>
+<?php perch_layout("global.footer") ?>
